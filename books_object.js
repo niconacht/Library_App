75ad96a664b6f46ee9f3d2b4ed67878eca152a25
Nico Nacht <nd.hagedorn@gmail.com>
2020-04-27 06:13:25 +0200

    structure

--- books_object.js
+++ books_object.js
@@ -1,11 +1,26 @@
-function Book(title, author, pages, read){
+
+let myLibrary = [];
+class Book {
+  constructor(title, author, pages, read) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
-  this.report = function(){
-    return `${title} by ${author}, ${pages}, ${read}`;
   }
-}
+  report() {
+    return return `${title} by ${author}, ${pages}, ${read}`;
+  }
+};
+
 const book1 = new Book ("Ulysses", "James Joyce", 1000, "read twice");
 console.log(book1.report());
+
+//button click open form, create new book instance
+//store in input in object
+//add object to array
+//display book in table
+
+
+function addBookToLibrary(input) {
+  // do stuff here
+}
