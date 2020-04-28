fa507501eae4a1f0b4483eb1babfc871fdaca240
Nicoletta <nicoletta.hirsch@gmail.com>
2020-04-28 08:19:54 +0200

    asdf

--- books_object.js
+++ books_object.js
@@ -36,14 +36,61 @@ function showBooks(libr){
       for (let [key, value] of Object.entries(obj)) {
         console.log(value);
     
-          const newEntry = document.createElement("td");
-          newEntry.textContent = value;
-          newRow.appendChild(newEntry);
+        const newEntry = document.createElement("td");
+          if (key === "read"){
+            let checkbox = document.createElement("input");
+            checkbox.id = "readToggle";
+            checkbox.setAttribute("type", "checkbox");
+            if (value ===  false) {
+              checkbox.checked = false;
+            }
+            
+            else {
+              checkbox.checked = true;
+            }
+            newEntry.appendChild(checkbox);
+          
+          }
+          else {
+            newEntry.textContent = value;
+          }
+          
+          newRow.appendChild(newEntry); 
 
       }
     }
 }
 
+const toggleRead = function(e) {
+  //find booktitle over parentnode, identify via index in Array, use index  to change read-status
+  if(e.target.id == "toggleRead"){
+      console.log("toggle")
+
+   }
+}
+//   let checked = e.target.checked;
+//   let bookTitle = e.target.parentNode[0];
+//   let bookName = bookTitle.textContent;
+//   let index = myLibrary.findIndex(bookTitle =>book.title === bookName);
+//   if(checked) {
+//     myLibrary.index.read = true;
+//   }
+//   else {
+//     myLibrary.index.read = false;
+//   }
+//   console.log(myLibrary);
+// }
+//EventHandlers
+
+
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("btn").addEventListener("click", bookToArray);
 });
+//if user changes checkbox update object
+document.addEventListener("change", toggleRead);
+
+//get eventhandler right
+//stop from repeated entries!!!
+//additional:
+//don't allow repeated entries
+//don't allow unrealistic number of pages
